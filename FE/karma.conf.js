5785c201d8e10bd5030fc6623eaf3f872f659c3b
WilhelmStu <willi.stuhle@gmail.com>
2022-12-28 17:49:06 +0100

    Added traceroute to workflow file

--- FE/karma.conf.js
+++ FE/karma.conf.js
@@ -63,12 +63,12 @@ module.exports = function(config) {
       subdir : 'coverage'
     },
 
-    junitReporter: {
-      outputDir: './reports/junit', // results will be saved as $outputDir/$browserName.xml
-      outputFile: 'test-report.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
-      testFilePattern: '.spec.ts',
-      useBrowserName: false // add browser name to report and classes names
-    },
+   // junitReporter: {
+   //   outputDir: './reports/junit', // results will be saved as $outputDir/$browserName.xml
+   //   outputFile: 'test-report.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
+   //   testFilePattern: '.spec.ts',
+   //   useBrowserName: false // add browser name to report and classes names
+   // },
 
 
     // web server port
